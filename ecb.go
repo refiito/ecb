246f4c5d855c66105b0a7ffedf12f3752fed192e
Margus Mahler <margus@toggl.com>
2015-04-29 17:38:10 +0300

    fetching rates from different url-s for speeds

--- ecb.go
+++ ecb.go
@@ -4,11 +4,17 @@ import (
 	"encoding/xml"
 	"strconv"
 	"time"
+  "fmt"
 
 	"github.com/refiito/timeoutclient"
 )
 
-// ECB XML envelope
+const (
+	dailyRatesXML     = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+	quarterlyRatesXML = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
+	allRatesXML       = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml"
+)
+
 type (
 	envelope struct {
 		Data []struct {
@@ -31,9 +37,55 @@ type (
 	}
 )
 
-func fetchXML() (result envelope, err error) {
+func RatesAt(date time.Time) (*ReferenceRate, error) {
+  xmlURL := allRatesXML
+  today := time.Now()
+
+  if isSameDay(date, today) {
+    xmlURL = dailyRatesXML
+  }
+  // 2190 hours is about three months...
+  if date.After(today.Add(-2190 * time.Hour)) {
+    xmlURL = quarterlyRatesXML
+  }
+  rates, err := fetchRates(xmlURL)
+  if err != nil {
+    return nil, err
+  }
+  
+  for _, rate := range rates {
+    if isSameDay(rate.Date, date) {
+      return &rate, nil
+    }
+  }
+  fmt.Println(date)
+  return nil, nil
+}
+
+func RateForAt(date time.Time, currency string) (*float64, error) {
+  rate, err := RatesAt(date)
+  if err != nil {
+    return nil, err
+  }
+  return rate.RateFor(currency), nil
+}
+
+func (rate *ReferenceRate) RateFor(currency string) *float64 {
+  for _, r := range rate.Rates {
+    if r.Currency == currency {
+      return &r.Rate
+    }
+  }
+  return nil
+}
+
+func isSameDay(t1, t2 time.Time) (result bool) {
+  return t1.Year() == t2.Year() && t1.Month() == t2.Month() && t1.Day() == t2.Day()
+}
+
+func fetchXML(xmlURL string) (result envelope, err error) {
 	client := timeoutclient.NewTimeoutClient(30*time.Second, 30*time.Second)
-	response, err := client.Get("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml")
+	response, err := client.Get(xmlURL)
 	if err != nil {
 		return
 	}
@@ -42,10 +94,10 @@ func fetchXML() (result envelope, err error) {
 	return
 }
 
-func FetchRates() ([]ReferenceRate, error) {
+func fetchRates(xmlURL string) ([]ReferenceRate, error) {
 	var result []ReferenceRate
 
-	data, err := fetchXML()
+	data, err := fetchXML(xmlURL)
 	if err != nil {
 		return result, err
 	}
--- ecb_test.go
+++ ecb_test.go
@@ -2,11 +2,29 @@ package ecb
 
 import (
 	"testing"
+	"time"
+	"errors"
 )
 
-func TestFetchRates(t *testing.T) {
-	_, err := FetchRates()
+func TestRatesAt(t *testing.T) {
+	rate, err := RatesAt(time.Date(2015, 1, 5, 12, 12, 12, 12, time.UTC))
 	if err != nil {
 		t.Error(err)
+	}
+	if rate == nil {
+		t.Error(errors.New("No rate found for 5th of January 2015"))
+	}
+	if *rate.RateFor("USD") != 1.1915 {
+		t.Error(errors.New("USD rate for 5th of January 2015 has changed..."))
+	}
+}
+
+func TestRateForAt(t *testing.T) {
+	rate, err := RateForAt(time.Date(2015, 1, 5, 12, 12, 12, 12, time.UTC), "USD")
+	if err != nil {
+		t.Error(err)
+	}
+	if *rate != 1.1915 {
+		t.Error(errors.New("USD rate for 5th of January 2015 has changed..."))	
 	}
 }
