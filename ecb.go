62b53737e862993dfc7cb0963c909b5223dbf2ff
Margus Mahler <margus@toggl.com>
2015-05-04 10:19:02 +0300

    FilledRatesForBetween function

--- ecb.go
+++ ecb.go
@@ -1,6 +1,7 @@
 package ecb
 
 import (
+	"errors"
 	"time"
 )
 
@@ -23,6 +24,22 @@ type (
 
 func RatesAt(date time.Time) (*ReferenceRate, error) {
 	return rateCache.ratesAt(date)
+}
+
+func CurrencyRateAt(date time.Time, currency string) (result CurrencyRate, err error) {
+	if date.Before(time.Date(1999, 1, 4, 0, 0, 0, 0, time.UTC)) {
+		err = errors.New("Date before data start")
+		return
+	}
+	rates, err := RatesAt(date)
+	if err != nil {
+		return
+	}
+	if rates == nil {
+		return CurrencyRateAt(date.AddDate(0, 0, -1), currency)
+	}
+	result = CurrencyRate{Date: date, Currency: currency, Rate: rates.RateFor(currency)}
+	return
 }
 
 func RateForAt(date time.Time, currency string) (*float64, error) {
@@ -48,6 +65,23 @@ func RatesForBetween(rangeStart, rangeEnd time.Time, currency string) ([]Currenc
 	return result, nil
 }
 
+func FilledRatesForBetween(rangeStart, rangeEnd time.Time, currency string) ([]CurrencyRate, error) {
+	var result []CurrencyRate
+	// Warm the cache, not caring about the return here, only the error
+	_, err := rateCache.ratesBetween(rangeStart, rangeEnd)
+	if err != nil {
+		return result, err
+	}
+	for checkDate := rangeStart; !isOneDayLater(checkDate, rangeEnd); checkDate.AddDate(0, 0, 1) {
+		rateAt, err := CurrencyRateAt(checkDate, currency)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, rateAt)
+	}
+	return result, err
+}
+
 func (rate *ReferenceRate) RateFor(currency string) *float64 {
 	for _, r := range rate.Rates {
 		if r.Currency == currency {
@@ -55,4 +89,8 @@ func (rate *ReferenceRate) RateFor(currency string) *float64 {
 		}
 	}
 	return nil
+}
+
+func isOneDayLater(t1, t2 time.Time) bool {
+	return isSameDay(t1.AddDate(0, 0, 1), t2)
 }
